8ccb7357fb81e2cd8d68d72d20b9c32e04c772ac
Peter Ajala <ajalapeter1@gmail.com>
2024-08-30 15:16:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation links
+// document.querySelectorAll('nav ul li a').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         const targetId = this.getAttribute('href').slice(0, -5); // Remove .html
+//         const targetSection = document.querySelector(targetId);
+//         targetSection.scrollIntoView({ behavior: 'smooth' });
+//     });
+// });
